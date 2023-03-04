b62a6e853dfdd6ca9d29361e3bd2fbc38a350f7f
banafshaahmadyar <ahmadyarbanafsha@gmail.com>
2023-03-04 11:04:52 +0000

    Added navbar from the header

--- contactapp/urls.py
+++ contactapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from contactapp import views
 
 urlpatterns = [
-    path('',views.index,name="index")
-
+    path('',views.index,name="index"),
+    path('about',views.about,name="about")
 ]

5359f365b4ec722ccefc5207ef1ce05d7cabc069
banafshaahmadyar <ahmadyarbanafsha@gmail.com>
2023-03-05 10:23:18 +0000

    create database table

--- contactapp/models.py
+++ contactapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Contact(models.Model):
+
+    name = models.CharField(max_length=25, blank=False, null=False)
+    lastname = models.CharField(max_length=25, blank=False, null=False)
+    number = models.IntegerField()
+    email = models.EmailField(max_length=25, blank=False, null=False)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=25, blank=False, null=False)
+
+    def __str__(self):
+        return self.name

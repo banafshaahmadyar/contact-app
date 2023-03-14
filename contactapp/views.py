442d0318cf3c48b08470eaaa0232efa9b99bc762
banafshaahmadyar <ahmadyarbanafsha@gmail.com>
2023-03-14 07:54:54 +0000

    created Updata page and it functionality

--- contactapp/views.py
+++ contactapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Contact
 
 # Create your views here.
@@ -7,7 +7,7 @@ from .models import Contact
 def index(request):
     data = Contact.objects.all()
     print(data)
-    context = {'data': data}
+    context = {"data": data}
     return render(request, "index.html", context)
 
 
@@ -24,7 +24,41 @@ def insertData(request):
         query = Contact(name=name, lastname=lastname,
                         number=number, email=email, age=age, gender=gender)
         query.save()
+        return redirect("/")
     return render(request, "index.html")
+
+
+def updateData(request, id):
+
+    if request.method == "POST":
+        name = request.POST['name']
+        lastname = request.POST['lastname']
+        number = request.POST['number']
+        email = request.POST['email']
+        age = request.POST['age']
+        gender = request.POST['gender']
+        edit = Contact.objects.get(id=id)
+        edit.name = name
+        edit.lastname = lastname
+        edit.number = number
+        edit.email = email
+        edit.age = age
+        edit.gender = gender
+        edit.save()
+        return redirect("/")
+
+    d = Contact.objects.get(id=id)
+    context = {"d": d}
+
+    return render(request, "edit.html", context)
+
+
+def deleteData(request, id):
+    data = Contact.objects.all()
+    print(data)
+    context = {"data": data}
+
+    return render(request, "index.html", context)
 
 
 def about(request):

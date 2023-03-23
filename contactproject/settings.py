04067398c209258e9f2e804d5c56c68b6ac3db5e
banafshaahmadyar <ahmadyarbanafsha@gmail.com>
2023-03-23 11:29:12 +0000

    added env.py file

--- contactproject/settings.py
+++ contactproject/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from django.contrib.messages import constants as messages
 from pathlib import Path
 
+from pathlib import Path
+import os
+import dj_database_url
+import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,11 +80,14 @@ WSGI_APPLICATION = 'contactproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 

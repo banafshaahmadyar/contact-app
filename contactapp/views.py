f9eb79d7823b840a803a01fa53396e184db85871
banafshaahmadyar <ahmadyarbanafsha@gmail.com>
2023-05-23 19:28:02 +0000

    added some functionalty to this app

--- contactapp/views.py
+++ contactapp/views.py
@@ -3,8 +3,9 @@ from .models import Contact
 from django.contrib import messages
 from django.conf import settings
 from django.http.response import HttpResponse
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -33,6 +34,7 @@ def insertData(request):
     return render(request, "index.html", context)
 
 
+@login_required
 def updateData(request, id):
 
     if request.method == "POST":
@@ -59,6 +61,7 @@ def updateData(request, id):
     return render(request, "edit.html", context)
 
 
+@login_required
 def deleteData(request, id):
     d = Contact.objects.get(id=id)
     d.delete()
@@ -67,62 +70,59 @@ def deleteData(request, id):
 
 
 def handleLogin(request):
-    if request.method=="POST":        
-        username=request.POST.get('username')
-        pass1=request.POST.get('pass1')
-        myuser=authenticate(username=username,password=pass1)
+    if request.method == "POST":
+        username = request.POST.get('username')
+        pass1 = request.POST.get('pass1')
+        myuser = authenticate(username=username, password=pass1)
         if myuser is not None:
-            login(request,myuser)
-            messages.success(request,"Login Successful")
+            login(request, myuser)
+            messages.success(request, "Login Successful")
             return redirect('/')
         else:
-            messages.error(request,"Invalid Credentials")
-            
-        
-    return render(request,"login.html")
+            messages.error(request, "Invalid Credentials")
+
+    return render(request, "login.html")
+
 
 def signup(request):
-    if request.method=="POST":
-        username=request.POST.get('username')
-        firstname=request.POST.get('fname')
-        lastname=request.POST.get('lname')
-        email=request.POST.get('email')
-        pass1=request.POST.get('pass1')
-        pass2=request.POST.get('pass2')
-        if pass1!=pass2:
-            messages.info(request,"Password is not Matching")
+    if request.method == "POST":
+        username = request.POST.get('username')
+        firstname = request.POST.get('fname')
+        lastname = request.POST.get('lname')
+        email = request.POST.get('email')
+        pass1 = request.POST.get('pass1')
+        pass2 = request.POST.get('pass2')
+        if pass1 != pass2:
+            messages.info(request, "Password is not Matching")
             return redirect('/signup')
-        
+
         try:
             if User.objects.get(username=username):
-                messages.warning(request,"UserName is Taken")
+                messages.warning(request, "UserName is Taken")
                 return redirect('/signup')
-           
+
         except Exception as identifier:
             pass
-        
-        
+
         try:
             if User.objects.get(email=email):
-                messages.warning(request,"Email is Taken")
+                messages.warning(request, "Email is Taken")
                 return redirect('/signup')
-           
+
         except Exception as identifier:
             pass
-        
-        
-        
-        myuser=User.objects.create_user(username,email,pass1)
-        myuser.first_name=firstname
-        myuser.last_name=lastname
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = firstname
+        myuser.last_name = lastname
         myuser.save()
-        messages.success(request,"User is Created Please Login")
+        messages.success(request, "User is Created Please Login")
         return redirect('/login')
-        
-        
-    return render(request,"signup.html")
+
+    return render(request, "signup.html")
+
 
 def handleLogout(request):
     logout(request)
-    messages.success(request,"Logout Success")    
+    messages.success(request, "Logout Success")
     return redirect('/login')

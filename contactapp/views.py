ea0f77e75fdc2cdfb8742fe639021d9070fb19ef
banafshaahmadyar <ahmadyarbanafsha@gmail.com>
2023-05-26 09:01:26 +0000

    add login function to index.html

--- contactapp/views.py
+++ contactapp/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-
+@login_required
 def index(request):
     data = Contact.objects.all()
     print(data)
